273cf245ff43658ea7fd734acb53106a19b29881
Rifki Arisagas <77217948+rifkisagas@users.noreply.github.com>
2022-12-16 15:40:56 +0700

    it didn't work but i'll try again later

--- app/src/main/java/com/example/studikasus_mobile5/MainActivity.kt
+++ app/src/main/java/com/example/studikasus_mobile5/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.studikasus_mobile5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.studikasus_mobile5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.Simulasi.setOnClickListener{
+            val intent = Intent(this, TambahActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
